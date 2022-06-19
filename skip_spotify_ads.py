124f585af3160a492c7df2d178b3e4bfdfcba077
Patrick Kantorski <ppkantorski@berkeley.edu>
2022-06-18 19:25:02 -0700

    Update skip_spotify_ads.py

--- skip_spotify_ads.py
+++ skip_spotify_ads.py
@@ -1,11 +1,21 @@
 from SwSpotify import spotify
 import os
 import time
+import datetime as dt
 import warnings
 warnings.simplefilter(action='ignore', category=UserWarning)
 
 
 SPOTIFY = '"Spotify"'
+
+print("===================================================================================")
+print("\
+   ▒█▀▀▀█ █░█ ░▀░ █▀▀█ 　 ▒█▀▀▀█ █▀▀█ █▀▀█ ▀▀█▀▀ ░▀░ █▀▀ █░░█ 　 ░█▀▀█ █▀▀▄ █▀▀ \n\
+   ░▀▀▀▄▄ █▀▄ ▀█▀ █░░█ 　 ░▀▀▀▄▄ █░░█ █░░█ ░░█░░ ▀█▀ █▀▀ █▄▄█ 　 ▒█▄▄█ █░░█ ▀▀█ \n\
+   ▒█▄▄▄█ ▀░▀ ▀▀▀ █▀▀▀ 　 ▒█▄▄▄█ █▀▀▀ ▀▀▀▀ ░░▀░░ ▀▀▀ ▀░░ ▄▄▄█ 　 ▒█░▒█ ▀▀▀░ ▀▀▀")
+print("===================================================================================")
+
+last = ('', '')
 while True:
     try:
         current = spotify.current()
@@ -16,5 +26,9 @@ while True:
         os.system('pkill -x Spotify')
         os.system('open /Applications/Spotify.app')
         os.system(f"osascript -e 'tell application {SPOTIFY} to play next track'")
+        print(f'[{dt.datetime.now()}] Ads have been skipped. :)')
+    elif current != ('', '') and last != current:
+        print(f'[{dt.datetime.now()}] Currently Playing: {current[0]} by {current[1]}')
     
+    last = current
     time.sleep(0.5)
